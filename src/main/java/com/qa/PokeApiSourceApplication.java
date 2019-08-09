de92b229d6b20e9a02cc25c87b0551ac82897ce1
lushilling <lshilling@hotmail.co.uk>
2019-08-09 09:57:01 +0100

    set up

---
+++ src/main/java/com/qa/PokeApiSourceApplication.java
@@ -0,0 +1,13 @@
+package com.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiSourceApplication.class, args);
+	}
+
+}
